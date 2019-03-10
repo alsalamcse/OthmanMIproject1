a1309a36bdf1289fe9c4f2ce2ca8cf06b56ebf22
Othman <otma.ctos.asslan@hotmail.com>
2019-03-10 12:12:56 +0200

    Othman's project

--- app/src/main/java/com/othman/markit/firstscreens/appscreens/GroupActivity.java
+++ app/src/main/java/com/othman/markit/firstscreens/appscreens/GroupActivity.java
@@ -4,15 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.othman.markit.R;
 
-public class GroupActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class GroupActivity extends AppCompatActivity {
+private ListView myGroupMembers;
+private ArrayAdapter<String > arrayAdapter;
+private ArrayList<String>arrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group);
+
+        myGroupMembers=(ListView)findViewById(R.id.GroupItemsList);
     }
 
     @Override
--- app/src/main/java/com/othman/markit/firstscreens/appscreens/GroupNameActivity.java
+++ app/src/main/java/com/othman/markit/firstscreens/appscreens/GroupNameActivity.java
@@ -1,5 +1,6 @@
 package com.othman.markit.firstscreens.appscreens;
 
+import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +11,13 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.othman.markit.R;
+import com.othman.markit.firstscreens.appscreens.groupAndItemsClasses.User;
 
 import java.util.ArrayList;
 
@@ -18,7 +25,8 @@ public class GroupNameActivity extends AppCompatActivity {
 private TextView textView;
 private ListView membersListView;
 private EditText groupName;
-
+private User GroupMember;
+DatabaseReference databaseReference;
 ArrayAdapter<String> adapter;
   ArrayList<String> list=new ArrayList<String>();
 
@@ -31,19 +39,25 @@ ArrayAdapter<String> adapter;
         textView=(TextView)findViewById(R.id.textView);
         groupName=(EditText)findViewById(R.id.editText2);
         adapter=new ArrayAdapter<String>(this,R.layout.activity_group_name,list);
-
+        databaseReference=FirebaseDatabase.getInstance().getReference();
         membersListView.setAdapter(adapter);
 
+        membersListView.addView(GroupMember);
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-
-
-
-
-
-
-
+            }
+        })
     }
 }
